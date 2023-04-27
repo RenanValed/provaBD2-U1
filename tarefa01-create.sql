fec639c0aad458a5b7afc266cef64af46ec13ef7
RenanValed <renanvaled@gmail.com>
2023-04-27 14:41:44 -0300

    ajuste na geracao automatica do codigo nas tabelas #1

--- tarefa01-create.sql
+++ tarefa01-create.sql
@@ -1,23 +1,28 @@
 CREATE TABLE funcionario (
-    codigo INT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(55) NOT NULL,
     sexo CHAR(1),
     dt_nasc DATE,
     salario DECIMAL(10,2),
-    cod_depto INT,
-    FOREIGN KEY (cod_depto) REFERENCES departamento(codigo)
+    cod_depto INT
 );
 
 CREATE TABLE departamento (
-    codigo INT PRIMARY KEY,
-    descricao VARCHAR(255) NOT NULL,
+    codigo INT PRIMARY KEY AUTO_INCREMENT,
+    descricao VARCHAR(55) NOT NULL,
     cod_gerente INT,
     FOREIGN KEY (cod_gerente) REFERENCES funcionario(codigo)
 );
 
+ALTER TABLE funcionario
+ADD CONSTRAINT fk_departamento_funcionario
+FOREIGN KEY (cod_depto)
+REFERENCES departamento(codigo);
+
+
 CREATE TABLE projeto (
     codigo INT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+    nome VARCHAR(55) NOT NULL,
     descricao TEXT,
     cod_depto INT,
     cod_responsavel INT,
@@ -29,7 +34,7 @@ CREATE TABLE projeto (
 
 CREATE TABLE atividade (
     codigo INT PRIMARY KEY,
-    nome VARCHAR(255) NOT NULL,
+    nome VARCHAR(55) NOT NULL,
     descricao TEXT,
     cod_responsavel INT,
     data_inicio DATE,
