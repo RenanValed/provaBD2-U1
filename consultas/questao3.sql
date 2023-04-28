af55dda30d56ed0cf19d6e75a33388845e857353
renanvaled <renanvaled@gmail.com>
2023-04-28 01:11:17 -0300

    questao 3 e 7 revisadas #1

--- consultas/questao3.sql
+++ consultas/questao3.sql
@@ -1,4 +1,8 @@
 --Faça uma consulta que selecione o nome e a data de nascimento dos funcionários com idade superior a 21 anos que não são gerentes.
 SELECT nome, dt_nasc
 FROM funcionario
-WHERE AGE(current_date, dt_nasc) > INTERVAL '21 years' AND codigo NOT IN (SELECT cod_gerente FROM departamento)
+WHERE EXTRACT(YEAR FROM age(NOW(), dt_nasc)) > 21 AND codigo NOT IN (
+  SELECT cod_gerente
+  FROM departamento
+  WHERE cod_gerente IS NOT NULL
+);
